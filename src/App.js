a0f992519a6e34a6f2b23059054b3206d8955b0f
natheen94 <natheenrls@gmail.com>
2022-02-15 12:57:39 +0530

    formvalidation

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { Signup } from './Components/SignUp';
+
+function App() {
+  return (
+    <div className="container">
+      <div className='row'>
+        <div className='col-md-6'>
+        <Signup/>
+
+        </div>
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
